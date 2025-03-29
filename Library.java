a3d225be837baec306009e894a1aa809952bca7e
Hanna <hwheeler@smith.edu>
2025-03-29 14:18:00 -0400

    library done

--- Library.java
+++ Library.java
@@ -1,12 +1,69 @@
 /* This is a stub for the Library class */
-public class Library {
 
-    public Library() {
-      System.out.println("You have built a library: 📖");
-    }
+import java.util.Hashtable;
+
+public class Library extends Building implements LibraryRequirements{
   
-    public static void main(String[] args) {
-      new Library();
+  //attributes
+  private Hashtable<String, Boolean> collection;
+
+  //constructor
+  public Library(String name, String address, int nFloors) {
+    super(name, address, nFloors);
+    this.collection = new Hashtable<String, Boolean>();
+    System.out.println("You have built a library: 📖");
+  }
+
+  //methods
+  public void addTitle(String title){
+    this.collection.put(title, true);
+  }
+
+  public String removeTitle(String title){
+    this.collection.remove(title);
+    return title;
+  }
+
+  public void checkOut(String title){
+    this.collection.replace(title, false);
+  }
+
+  public void returnBook(String title){
+    this.collection.replace(title,true);
+  }
+
+  public boolean containsTitle(String title){ // returns true if the title appears as a key in the Libary's collection, false otherwise
+    if (this.collection.containsKey(title)){
+      return true;
+    } else{
+      return false;
     }
+  } 
+  
+  public boolean isAvailable(String title){ // returns true if the title is currently available, false otherwise
+    if (this.collection.get(title)){
+      return true;
+    } else{
+      return false;
+    }
+  } 
+  
+  public void printCollection(){// prints out the entire collection in an easy-to-read way (including checkout status)
+    System.out.println("Collection:");
+    this.collection.forEach(
+      (k,v)-> System.out.println("Title: " + k + "; Available for checkout: " + v));
+
+  } 
+  
+  public static void main(String[] args) {
+    Library library = new Library("Neilson", "7 Neilson Drive", 4);
+    library.addTitle("The Lorax by Dr. Seuss");
+    library.printCollection();
+    library.addTitle("The Bell Jar by Sylvia Plath");
+    library.checkOut("The Lorax by Dr. Seuss");
+    library.printCollection();
+    library.returnBook("The Lorax by Dr. Seuss");
+    library.printCollection();
+  }
   
   }
